dd672e15845b082ad511d600cfda3e73de5d9bf4
thomas <tcarr2@nd.edu>
2021-09-30 12:49:58 -0400

    print statement after class

--- scratch/Journal.py
+++ scratch/Journal.py
@@ -2,4 +2,11 @@ class Person:
     def __init__(self):
         pass
 
+class Car:
+    def __init__(self, speed = 0):
+        self.speed = speed
+        self.mileage = 0
+    print('The car is moving {} mph'.format(self.speed))
+
+
 

6221df142ead45b62415b5f68e06753358debbba
thomas <tcarr2@nd.edu>
2021-10-04 17:05:01 -0400

    added other 3 family members

--- family.py
+++ family.py
@@ -7,3 +7,7 @@ class FamilyMember:
 familymember1 = FamilyMember("Natalie", "mother", 68)
 
 print("My {} is named {} and is {} inches tall.").format(familymember1.relationship, familymember1.name, familymember1.height)
+
+familymember2 = FamilyMember("Tim", "father", 70)
+familymember3 = FamilyMember("Connor", "brother", 72)
+familymember4 = FamilyMember("Darby", "sister", 68)
